ae9eff158ca484eead924d7bf3dc5f2a8d782d6e
purplehorse5230 <105677069+purplehorse5230@users.noreply.github.com>
2024-03-01 11:52:08 -0500

    Delete PolyExponential copy directory
    Updating

--- PolyExponential copy/analsolu.m
+++
@@ -1,7 +0,0 @@
-function res=analsolu(t,H)
-
-% The analytical solution at time t.
-res=expm(t*H);
-    
-return
-
--- PolyExponential copy/tgauss.m
+++
@@ -1,4 +0,0 @@
-function [A,B,tc]=tgauss(n);
-%A=zeros(n+1,n+1);
-eval(strcat('tgauss',num2str(n)));
-return
